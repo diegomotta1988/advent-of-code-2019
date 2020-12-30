08098e90e400dfa9aa162b9b6025399b627e0302
diegomotta1988 <diegomottaalvarez@gmail.com>
2020-12-30 08:13:55 +0100

    day 4

--- day4/index.js
+++ day4/index.js
@@ -1,2 +1,28 @@
 const input = `153517-630395`;
 const [firstNumber, lastNumber] = input.split('-');
+const repeatedDigitRegExp = /([\d])\1{1}/;
+
+const moreThan2RepeatedRegExp = /([\d])\1{2,3}/;
+
+let count = 0;
+for (let i = firstNumber; i < lastNumber; i++) {
+  if (passesCondition(i.toString())) {
+    count++;
+  }
+}
+console.log(count);
+
+function passesCondition(number) {
+  const match = number.match(moreThan2RepeatedRegExp);
+
+  const stringToCheck = match ? number.replace(match[0], '') : number;
+  if (!repeatedDigitRegExp.test(stringToCheck)) {
+    return false;
+  }
+  for (let j = 0; j < number.length; j++) {
+    if (number[j] < number[j - 1]) {
+      return false;
+    }
+  }
+  return true;
+}
